b0ce8aee47fce5c41b07be455b3085ce2f40ad9b
Benjamin23k <wilsonbenjaminrosa@gmail.com>
2026-06-05 20:29:07 -0400

    Add MiembroDeLaComunidad base class
    Defines shared attributes and behaviors for all community members

---
+++ task 1/Entities/Miembro_Comunidad.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace task_Entities
+{
+
+   public class miembro_comunidad
+   {
+      public int id { get; set; }
+      public string nombre { get; set; } = string.Empty;
+
+   }
+
+
+
+
+}
+
