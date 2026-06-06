f24995c6923cde570109d5730f07d6e9e470b091
Benjamin23k <wilsonbenjaminrosa@gmail.com>
2026-06-05 20:36:31 -0400

    Add Docente and Administrativo classes
    Both classes inherit from Empleado

---
+++ task 1/Entities/administrativo.cs
@@ -0,0 +1,22 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace task_Entities
+{
+
+  public class Administrado : Empleado
+  {
+    public string departamento { get; set; } = string.Empty;
+
+    public string gmail { get; set; } = string.Empty;
+  }
+
+
+}
+
+
---
+++ task 1/Entities/docente.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace task_Entities
+{
+
+   public class Docente : Empleado
+   {
+      public string asignatura { get; set; } = string.Empty;
+
+   }
+
+
+}
+
+
