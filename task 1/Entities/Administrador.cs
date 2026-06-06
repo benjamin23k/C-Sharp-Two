d3669290ccb85d3f6ca9132146b864224927e1ac
Benjamin23k <wilsonbenjaminrosa@gmail.com>
2026-06-05 20:37:40 -0400

    Add Administrador and Maestro classes
    Both classes inherit from Docente

---
+++ task 1/Entities/Administrador.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace task_Entities
+{
+  public class Administrador : Docente
+  {
+    public DateTime Fechaingreso { get; set; }
+
+  }
+
+}
+
---
+++ task 1/Entities/Maestro.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace task_Entities
+{
+  public class Maestro : Docente
+  {
+    public int experiencia { get; set; }
+  }
+}
+
